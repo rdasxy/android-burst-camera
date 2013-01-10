1687135dcd002c48549dfe1eeb4347e5dbcc2125
Riddhiman Das <rdasxy@gmail.com>
2013-01-10 16:52:52 -0600

    Initial commit

---
+++ src/com/example/CameraDemo.java
@@ -0,0 +1,97 @@
+package com.example;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class CameraDemo extends Activity {
+	private static final String TAG = "FrontCamera";
+	Camera camera;
+	Preview preview;
+	Button buttonClick;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		preview = new Preview(this);
+		((FrameLayout) findViewById(R.id.preview)).addView(preview);
+
+		buttonClick = (Button) findViewById(R.id.buttonClick);
+		buttonClick.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+//				for (int i = 0; i < 5; i++) {
+					preview.camera.takePicture(shutterCallback, rawCallback,
+							jpegCallback);
+//				}
+			}
+		});
+
+		Log.d(TAG, "onCreate'd");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (camera != null) {
+			camera.release();
+		}
+	}
+
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+			Log.d(TAG, "onShutter'd");
+		}
+	};
+
+	/** Handles data for raw picture */
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+
+	/** Handles data for jpeg picture */
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				// write to local sandbox file system
+				// outStream =
+				// CameraDemo.this.openFileOutput(String.format("%d.jpg",
+				// System.currentTimeMillis()), 0);
+				// Or write to sdcard
+				outStream = new FileOutputStream(String.format(
+						"/sdcard/eyeverify/still%d.jpg",
+						System.currentTimeMillis()));
+				outStream.write(data);
+				outStream.close();
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			Log.d(TAG, "onPictureTaken - jpeg");
+			try {
+				camera.startPreview();
+			} catch (Exception e) {
+				Log.d(TAG, "Error starting preview: " + e.toString());
+			}
+		}
+	};
+
+}
---
+++ src/com/example/Preview.java
@@ -0,0 +1,89 @@
+package com.example;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+class Preview extends SurfaceView implements SurfaceHolder.Callback {
+	private static final String TAG = "Preview";
+
+    SurfaceHolder mHolder;
+    public Camera camera;
+    
+    Preview(Context context) {
+        super(context);
+        
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+        camera = Camera.open();
+        try {
+			camera.setPreviewDisplay(holder);
+			
+			
+			camera.setPreviewCallback(new PreviewCallback() {
+
+				public void onPreviewFrame(byte[] data, Camera arg1) {
+					FileOutputStream outStream = null;
+					try {
+						outStream = new FileOutputStream(String.format("/sdcard/eyeverify/preview%d.jpg", System.currentTimeMillis()));	
+						outStream.write(data);
+						outStream.close();
+						Log.d(TAG, "onPreviewFrame - wrote bytes: " + data.length);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+					}
+						Preview.this.invalidate();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        camera.stopPreview();
+        camera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+//        Camera.Parameters parameters = camera.getParameters();
+//        parameters.setPreviewSize(w, h);
+//        camera.setParameters(parameters);
+        camera.startPreview();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    		super.draw(canvas);
+    		Paint p= new Paint(Color.RED);
+    		Log.d(TAG,"draw");
+    		canvas.drawText("PREVIEW", canvas.getWidth()/2, canvas.getHeight()/2, p );
+    }
+}
